a1d73c7dc70099c5217ed3f91034ecfb9fff1d47
dariaprikhodko <favour313@mail.ru>
2018-12-10 23:25:57 +1100

    исправления по заданию Пока все дома

--- js/main.js
+++ js/main.js
@@ -7,6 +7,39 @@ var CHECK_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 // var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var AVATAR = [1, 2, 3, 4, 5, 6, 7, 8];
+var CARDS_AMOUNT = 8;
+
+// ищем на странице блок .map--faded
+var mapElement = document.querySelector('.map--faded');
+
+// убираем класс .map--faded у блока map
+
+var showMapElement = function () {
+  showMapElement.classList.remove('.map--faded');
+};
+
+// находим и выносим в переменную блок .map
+
+var similarCardElement = mapElement.querySelector('.map__card');
+
+// находим и выносим в переменную блок .map__filters-container, чтобы вставит карточки перед ним
+var filtersContainerElement = document.querySelector('.map__filters-container');
+
+// заполняем шаблон #card
+
+var similarCardTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.map__card popup');
+
+// находим и выносим в переменную блок .map__pins
+
+var similarPinElement = mapElement.querySelector('.map__pins');
+
+// заполняем шаблон #pin
+
+var similarPinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
 
 // Вспомогательные функции по поиску случайных чисел
 
@@ -25,16 +58,16 @@ var getRandomAmount = function (min, max) {
 
 // Создаем массив, состоящий из 8 сгенерированных JS объектов, которые будут описывать похожие объявления неподалёку.
 
-var generateAd = function () {
+var generateAds = function () {
   var ads = [];
-  for (var i = 0; i < 8; i++) {
+  for (var i = 0; i < CARDS_AMOUNT; i++) {
     ads.push({
       author: {
         avatar: ('img/avatars/user' + '0' + getRandomNum(AVATAR) + '.png'),
       },
       offer: {
         title: getRandomNum(TITLE),
-        address: '{{location.x}}, {{location.y}}',
+        address: '',
         price: getRandomAmount(1000, 1000000),
         type: getRandomNum(TYPE),
         rooms: getRandomNum(ROOMS),
@@ -54,38 +87,19 @@ var generateAd = function () {
   return ads;
 };
 
-// ищем на странице блок .map--faded
-var mapElement = document.querySelector('.map--faded');
-
-// убираем класс .map--faded у блока map
-
-var showMapElement = function () {
-  showMapElement.classList.remove('.map--faded');
-};
-
-// заполняем шаблон #pin
-
-var similarPinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-
 // создаем DOM-элементы, соответствующие меткам на карте, и заполняем их данными из массива.
 
 var renderPin = function (ads) {
   var pinElement = similarPinTemplate.cloneNode(true);
 
   pinElement.querySelector('.map__pin').style = 'left:' + ads.x + 'px;' + 'top:' + ads.y + 'px;';
-  pinElement.querySelector('.map__pin').src = ads.avatar;
-  pinElement.querySelector('.map__pin').alt = 'Заголовок объявления';
+  pinElement.querySelector('img').src = ads.avatar;
+  pinElement.querySelector('img').alt = 'Заголовок объявления';
 
   return pinElement;
 };
 
 // отрисовываем сгенерированные DOM-элементы в блок .map__pins.
-
-// находим и выносим в переменную блок .map__pins
-
-var similarPinElement = mapElement.querySelector('.map__pins');
 
 var renderPins = function (ads) {
   var fragment = document.createDocumentFragment();
@@ -94,12 +108,6 @@ var renderPins = function (ads) {
   }
   similarPinElement.appendChild(fragment);
 };
-
-// заполняем шаблон #card
-
-var similarCardTemplate = document.querySelector('#card')
-    .content
-    .querySelector('.map__card popup');
 
 // создаем DOM-элементы объявлений
 
@@ -121,13 +129,6 @@ var renderCard = function (ads) {
 
 // отрисовываем сгенерированные DOM-элементы в блок .map.
 
-// находим и выносим в переменную блок .map
-
-var similarCardElement = mapElement.querySelector('.map__card');
-
-// находим и выносим в переменную блок .map__filters-container, чтобы вставит карточки перед ним
-var filtersContainerElement = document.querySelector('.map__filters-container');
-
 var renderCards = function (ads) {
   var card = document.createDocumentFragment();
   for (var i = 0; i < ads.length; i++) {
@@ -140,12 +141,12 @@ var renderCards = function (ads) {
 
 // создаем обобщающую функцию
 
-var init = function (ads) {
-  generateAd();
-  renderPin(ads);
-  renderPins(ads);
-  renderCard(ads);
-  renderCards(ads);
+var init = function () {
+  generateAds();
+  renderPin();
+  renderPins();
+  renderCard();
+  renderCards();
 };
 
 init();
